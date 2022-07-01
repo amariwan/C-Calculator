4b6a3e523b171f9ddc6fb2fbf710181513205ce2
Aland Mariwan <dev@aland-mariwan.de>
2022-07-01 08:54:28 +0000

    fixed

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -8,46 +8,57 @@ using System.Threading.Tasks;
 namespace taschenrechner {
 	class Program {
 		static void Main(string[] args) {
-			Console.Clear();
+			Console.Clear();		
 			Console.WriteLine("Hey there! welcome to Aland Calculator.\n");
 			Console.WriteLine("Please enter an operand (+, -, /, *) \n");
-			Console.WriteLine("Enter the action to be performed: ");  
-			string input = Console.ReadLine();
-			string num1 = "";
-			string num2 = "";
-			int type = 0;
-			char operand = ' ';
-			float answer = 0;
 			
-			for (int i = 0; i < input.Length; i++)
+			while (true)
 			{
-				if (input[i] >= '0' && input[i] <= '9' && type == 0) {
-					num1 += input[i];
-					
-				} else if (input[i] == '+' || input[i] == '-' || input[i] == '/'|| input[i] == '*') {
-					operand = input[i];
-					type = 1;
-					
-				} else if (input[i] >= '0' && input[i] <= '9') {
-					num2 += input[i];
-					
-				} else {
-					Console.WriteLine(input[i] + " crach");
-					
+				Console.WriteLine("Enter the action to be performed: ");  
+				string input = Console.ReadLine();
+				string num1 = "";
+				string num2 = "";
+				int type = 0;
+				char operand = ' ';
+				float answer = 0;
+				
+				if (input == "exit")
+				{
+					Console.WriteLine("Bye, have a nice day ");  
+					break;
 				}
+				
+				for (int i = 0; i < input.Length; i++)
+				{
+					if (input[i] >= '0' && input[i] <= '9' && type == 0) {
+						num1 += input[i];
+						
+					} else if (input[i] == '+' || input[i] == '-' || input[i] == '/'|| input[i] == '*') {
+						operand = input[i];
+						type = 1;
+						
+					} else if (input[i] >= '0' && input[i] <= '9') {
+						num2 += input[i];
+						
+					} else {
+						Console.WriteLine(input[i] + " crach");
+						
+					}
+				}
+				
+				int number1 = int.Parse(num1);
+				int number2 = int.Parse(num2);
+				
+				
+				switch (operand) {
+					case '+' : answer = number1 + number2; break;
+					case '-' : answer = number1 - number2; break;
+					case '*' : answer = number1 * number2; break;
+					case '/' : answer = number1 / number2; break;
+				}
+				Console.WriteLine(num1 + "" + operand + "" + num2);
+				Console.WriteLine(answer); 
 			}
-
-			int number1 = Int32.Parse(num1);
-			int number2 = Int32.Parse(num2);
-			
-			switch (operand) {
-				case '+' : answer = number1 + number2; break;
-				case '-' : answer = number1 - number2; break;
-				case '*' : answer = number1 * number2; break;
-				case '/' : answer = number1 / number2; break;
-			}
-			Console.WriteLine(num1 + "" + operand + "" + num2);
-			Console.WriteLine(answer); 
 		}
 	}
 }
